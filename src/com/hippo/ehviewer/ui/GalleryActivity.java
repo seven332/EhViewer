0631c2925a9cbc469abe2c02171ca06b06d46072
seven332 <seven332@163.com>
2014-11-12 23:55:11 +0800

    调整阅读界面滑调消失显示动画

--- src/com/hippo/ehviewer/ui/GalleryActivity.java
+++ src/com/hippo/ehviewer/ui/GalleryActivity.java
@@ -86,26 +86,30 @@ public class GalleryActivity extends Activity
     void setFullScreen(final boolean fullScreen) {
         mFullScreenHelper.setFullScreen(fullScreen);
 
-        AlphaAnimation aa = fullScreen ? new AlphaAnimation(1.0f, 0.0f) : new AlphaAnimation(0.0f, 1.0f);
-        aa.setDuration(Constants.ANIMATE_TIME);
-        aa.setAnimationListener(new AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-                mFooter.setVisibility(View.VISIBLE);
-            }
+        if (!(fullScreen && mFooter.getVisibility() == View.GONE) &&
+                !(!fullScreen && mFooter.getVisibility() == View.VISIBLE)) {
+            AlphaAnimation aa = fullScreen ? new AlphaAnimation(1.0f, 0.0f) :
+                    new AlphaAnimation(0.0f, 1.0f);
+            aa.setDuration(Constants.ANIMATE_TIME);
+            aa.setAnimationListener(new AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    mFooter.setVisibility(View.VISIBLE);
+                }
 
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                if (fullScreen)
-                    mFooter.setVisibility(View.INVISIBLE);
-            }
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    if (fullScreen)
+                        mFooter.setVisibility(View.GONE);
+                }
 
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-                // Empty
-            }
-        });
-        mFooter.startAnimation(aa);
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+                    // Empty
+                }
+            });
+            mFooter.startAnimation(aa);
+        }
 
         if (!fullScreen)
             startFullScreenTask();
