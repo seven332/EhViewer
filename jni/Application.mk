2dcbdb2e1279c4106a96231227943cd0510346c2
seven332 <seven332@163.com>
2014-11-26 02:43:51 +0800

    添加 arm64 支持

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,4 +1,4 @@
 # x86 libjpeg-turbo 汇编链接会出错
-APP_ABI := armeabi armeabi-v7a# arm64-v8a# mips# x86
+APP_ABI := armeabi armeabi-v7a arm64-v8a# mips# x86
 APP_PLATFORM := android-14
 APP_OPTIM := release
