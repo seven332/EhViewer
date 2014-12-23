4b9f872cf63f0fce559c71c063cd9505c85e123c
seven332 <seven332@163.com>
2014-12-23 15:48:14 +0800

    update

--- src/com/hippo/ehviewer/StartActivity.java
+++ src/com/hippo/ehviewer/StartActivity.java
@@ -202,23 +202,6 @@ public class StartActivity extends AbsActivity {
             }
         }, 2000);
         check(CHECK_WARING);
-
-
-
-
-        /*
-        HttpHelper hh = new HttpHelper(StartActivity.this);
-        hh.downloadInThread("http://exhentai.org/fullimg.php?gid=767110&page=15&key=84c2b5f9aa", new File("/sdcard"), "aaa.txt", false, null, null);
-
-        new Thread() {
-            @Override
-            public void run() {
-
-
-                //
-            }
-        }.start();
-*/
     }
 
     /**
