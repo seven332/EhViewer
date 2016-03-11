8badf7aad76d4012c9315d9965e141247927c420
seven332 <seven332@163.com>
2016-03-11 14:23:00 +0800

    Fix arg type error

--- app/src/main/java/com/hippo/ehviewer/client/EhUrlOpener.java
+++ app/src/main/java/com/hippo/ehviewer/client/EhUrlOpener.java
@@ -63,7 +63,7 @@ public class EhUrlOpener {
             args.putString(ProgressScene.KEY_ACTION, ProgressScene.ACTION_GALLERY_TOKEN);
             args.putLong(ProgressScene.KEY_GID, result2.gid);
             args.putString(ProgressScene.KEY_PTOKEN, result2.pToken);
-            args.putLong(ProgressScene.KEY_PAGE, result2.page);
+            args.putInt(ProgressScene.KEY_PAGE, result2.page);
             return new Announcer(ProgressScene.class).setArgs(args);
         }
 
--- app/src/main/java/com/hippo/ehviewer/ui/scene/ProgressScene.java
+++ app/src/main/java/com/hippo/ehviewer/ui/scene/ProgressScene.java
@@ -103,7 +103,7 @@ public final class ProgressScene extends BaseScene implements View.OnClickListen
 
         mAction = args.getString(KEY_ACTION);
         if (ACTION_GALLERY_TOKEN.equals(mAction)) {
-            mGid = args.getInt(KEY_GID, -1);
+            mGid = args.getLong(KEY_GID, -1);
             mPToken = args.getString(KEY_PTOKEN, null);
             mPage = args.getInt(KEY_PAGE, -1);
             if (mGid == -1 || mPToken == null || mPage == -1) {
