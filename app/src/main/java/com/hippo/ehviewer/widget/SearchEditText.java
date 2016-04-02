f50975d767302ff8c7b920f37f155afdd1833650
seven332 <seven332@163.com>
2016-04-02 23:11:56 +0800

    Adjust search bar size

--- app/src/main/java/com/hippo/ehviewer/widget/SearchEditText.java
+++ app/src/main/java/com/hippo/ehviewer/widget/SearchEditText.java
@@ -18,12 +18,12 @@ package com.hippo.ehviewer.widget;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.AppCompatEditText;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
-import android.widget.EditText;
 
-public class SearchEditText extends EditText {
+public class SearchEditText extends AppCompatEditText {
 
     private SearchEditTextListener mListener;
 
