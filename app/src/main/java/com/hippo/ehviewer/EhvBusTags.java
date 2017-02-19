0c3c531a47f43466ad5203a4274e4652d08d8c4e
seven332 <seven332@163.com>
2017-02-19 19:55:00 +0800

    Add EhvBusTags

---
+++ app/src/main/java/com/hippo/ehviewer/EhvBusTags.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2017 Hippo Seven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hippo.ehviewer;
+
+/*
+ * Created by Hippo on 2/14/2017.
+ */
+
+public final class EhvBusTags {
+  private EhvBusTags() {}
+
+  /**
+   * Sign in.
+   *
+   * Pair<String, String>(name, avatar)
+   */
+  public static final String TAG_SIGN_IN = "sign_in";
+
+  /**
+   * Sign out.
+   *
+   * Void
+   */
+  public static final String TAG_SIGN_OUT = "sign_out";
+}
