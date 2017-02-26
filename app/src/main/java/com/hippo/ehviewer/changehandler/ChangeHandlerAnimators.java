bee0c6f7c063eab0a9f696d0c72ff286a1e7dafb
seven332 <seven332@163.com>
2017-02-27 00:41:29 +0800

    Add ChangeHandlerAnimators.changeBounds()

--- app/src/main/java/com/hippo/ehviewer/changehandler/ChangeHandlerAnimators.java
+++ app/src/main/java/com/hippo/ehviewer/changehandler/ChangeHandlerAnimators.java
@@ -21,12 +21,15 @@ package com.hippo.ehviewer.changehandler;
  */
 
 import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
+import android.graphics.PointF;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.graphics.drawable.DrawerArrowDrawable;
 import android.util.Property;
 import android.view.View;
@@ -37,6 +40,10 @@ import com.hippo.ehviewer.util.FloatProperty;
 import com.hippo.ehviewer.util.IntProperty;
 import com.hippo.ehviewer.widget.ControllerContainer;
 import com.hippo.yorozuya.android.AnimationUtils;
+import com.transitionseverywhere.ArcMotion;
+import com.transitionseverywhere.utils.AnimatorUtils;
+import com.transitionseverywhere.utils.PointFProperty;
+import com.transitionseverywhere.utils.ViewUtils;
 
 public final class ChangeHandlerAnimators {
   private ChangeHandlerAnimators() {}
@@ -169,5 +176,164 @@ public final class ChangeHandlerAnimators {
         to, DRAWER_ARROW_DRAWABLE_PROGRESS,
         from.getProgress(), to.getProgress()
     );
+  }
+
+
+  ///////////////////////////////////////////////////////////////////////////
+  // Change bounds
+  ///////////////////////////////////////////////////////////////////////////
+
+  private static final PointFProperty<ViewBounds> TOP_LEFT_PROPERTY;
+  private static final PointFProperty<ViewBounds> BOTTOM_RIGHT_PROPERTY;
+  private static final PointFProperty<View> BOTTOM_RIGHT_ONLY_PROPERTY;
+  private static final PointFProperty<View> TOP_LEFT_ONLY_PROPERTY;
+  private static final PointFProperty<View> POSITION_PROPERTY;
+  private static final ArcMotion ACR_PATH_MOTION;
+
+  static {
+    TOP_LEFT_PROPERTY = new PointFProperty<ViewBounds>() {
+      @Override
+      public void set(ViewBounds viewBounds, PointF topLeft) {
+        viewBounds.setTopLeft(topLeft);
+      }
+    };
+    BOTTOM_RIGHT_PROPERTY = new PointFProperty<ViewBounds>() {
+      @Override
+      public void set(ViewBounds viewBounds, PointF bottomRight) {
+        viewBounds.setBottomRight(bottomRight);
+      }
+    };
+    BOTTOM_RIGHT_ONLY_PROPERTY = new PointFProperty<View>() {
+      @Override
+      public void set(View view, PointF bottomRight) {
+        int left = view.getLeft();
+        int top = view.getTop();
+        int right = Math.round(bottomRight.x);
+        int bottom = Math.round(bottomRight.y);
+        ViewUtils.setLeftTopRightBottom(view, left, top, right, bottom);
+      }
+    };
+    TOP_LEFT_ONLY_PROPERTY = new PointFProperty<View>() {
+      @Override
+      public void set(View view, PointF topLeft) {
+        int left = Math.round(topLeft.x);
+        int top = Math.round(topLeft.y);
+        int right = view.getRight();
+        int bottom = view.getBottom();
+        ViewUtils.setLeftTopRightBottom(view, left, top, right, bottom);
+      }
+    };
+    POSITION_PROPERTY = new PointFProperty<View>() {
+      @Override
+      public void set(View view, PointF topLeft) {
+        int left = Math.round(topLeft.x);
+        int top = Math.round(topLeft.y);
+        int right = left + view.getWidth();
+        int bottom = top + view.getHeight();
+        ViewUtils.setLeftTopRightBottom(view, left, top, right, bottom);
+      }
+    };
+    ACR_PATH_MOTION = new ArcMotion();
+    ACR_PATH_MOTION.setMaximumAngle(90);
+    ACR_PATH_MOTION.setMinimumHorizontalAngle(15);
+    ACR_PATH_MOTION.setMinimumVerticalAngle(0);
+  }
+
+  @Nullable
+  public static Animator changeBounds(@Nullable View from, @Nullable View to) {
+    if (from == null || !ViewCompat.isLaidOut(from) || from.getWidth() == 0 || from.getHeight() == 0
+        || to == null || !ViewCompat.isLaidOut(to) || to.getWidth() == 0 || to.getHeight() == 0) {
+      return null;
+    }
+    ViewGroup fromParent = (ViewGroup) from.getParent();
+    ViewGroup toParent = (ViewGroup) to.getParent();
+    if (fromParent == null || toParent == null) {
+      return null;
+    }
+    final int startLeft = from.getLeft();
+    final int endLeft = to.getLeft();
+    final int startTop = from.getTop();
+    final int endTop = to.getTop();
+    final int startRight = from.getRight();
+    final int endRight = to.getRight();
+    final int startBottom = from.getBottom();
+    final int endBottom = to.getBottom();
+    final int startWidth = startRight - startLeft;
+    final int startHeight = startBottom - startTop;
+    final int endWidth = endRight - endLeft;
+    final int endHeight = endBottom - endTop;
+    int numChanges = 0;
+    if ((startWidth != 0 && startHeight != 0) || (endWidth != 0 && endHeight != 0)) {
+      if (startLeft != endLeft || startTop != endTop) ++numChanges;
+      if (startRight != endRight || startBottom != endBottom) ++numChanges;
+    }
+    if (numChanges > 0) {
+      Animator anim;
+      ViewUtils.setLeftTopRightBottom(to, startLeft, startTop, startRight, startBottom);
+      if (numChanges == 2) {
+        if (startWidth == endWidth && startHeight == endHeight) {
+          anim = AnimatorUtils.ofPointF(to, POSITION_PROPERTY, ACR_PATH_MOTION,
+              startLeft, startTop, endLeft, endTop);
+        } else {
+          ViewBounds viewBounds = new ViewBounds(to);
+          Animator topLeftAnimator = AnimatorUtils.ofPointF(viewBounds,
+              TOP_LEFT_PROPERTY, ACR_PATH_MOTION,
+              startLeft, startTop, endLeft, endTop);
+          Animator bottomRightAnimator = AnimatorUtils.ofPointF(viewBounds,
+              BOTTOM_RIGHT_PROPERTY, ACR_PATH_MOTION,
+              startRight, startBottom, endRight, endBottom);
+          AnimatorSet set = new AnimatorSet();
+          set.playTogether(topLeftAnimator, bottomRightAnimator);
+          set.addListener(viewBounds);
+          anim = set;
+        }
+      } else if (startLeft != endLeft || startTop != endTop) {
+        anim = AnimatorUtils.ofPointF(to, TOP_LEFT_ONLY_PROPERTY, null,
+            startLeft, startTop, endLeft, endTop);
+      } else {
+        anim = AnimatorUtils.ofPointF(to, BOTTOM_RIGHT_ONLY_PROPERTY, null,
+            startRight, startBottom, endRight, endBottom);
+      }
+      return anim;
+    }
+    return null;
+  }
+
+  private static class ViewBounds extends AnimatorListenerAdapter {
+    private int left;
+    private int top;
+    private int right;
+    private int bottom;
+    private boolean isTopLeftSet;
+    private boolean isBottomRightSet;
+    private View view;
+
+    public ViewBounds(View view) {
+      this.view = view;
+    }
+
+    public void setTopLeft(PointF topLeft) {
+      left = Math.round(topLeft.x);
+      top = Math.round(topLeft.y);
+      isTopLeftSet = true;
+      if (isBottomRightSet) {
+        setLeftTopRightBottom();
+      }
+    }
+
+    public void setBottomRight(PointF bottomRight) {
+      right = Math.round(bottomRight.x);
+      bottom = Math.round(bottomRight.y);
+      isBottomRightSet = true;
+      if (isTopLeftSet) {
+        setLeftTopRightBottom();
+      }
+    }
+
+    private void setLeftTopRightBottom() {
+      ViewUtils.setLeftTopRightBottom(view, left, top, right, bottom);
+      isTopLeftSet = false;
+      isBottomRightSet = false;
+    }
   }
 }
