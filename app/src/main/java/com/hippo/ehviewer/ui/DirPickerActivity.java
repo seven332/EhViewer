2124a50547a3410acea4e3a20b16b92d9496a8e8
0w0@0w0.pw <0w0@0w0.pw>
2018-02-10 18:35:08 +0800

    Clean up the import

--- app/src/main/java/com/hippo/ehviewer/ui/DirPickerActivity.java
+++ app/src/main/java/com/hippo/ehviewer/ui/DirPickerActivity.java
@@ -23,12 +23,10 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.hippo.ehviewer.R;
-import com.hippo.ehviewer.Settings;
 import com.hippo.ripple.Ripple;
 import com.hippo.widget.DirExplorer;
 import com.hippo.yorozuya.ViewUtils;
