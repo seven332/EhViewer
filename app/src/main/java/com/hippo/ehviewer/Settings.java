e8c06fd8767c7ffb44d41f5719205466f200c419
seven332 <seven332@163.com>
2018-10-17 23:07:01 +0800

    Add theme setting

--- app/src/main/java/com/hippo/ehviewer/Settings.java
+++ app/src/main/java/com/hippo/ehviewer/Settings.java
@@ -237,6 +237,17 @@ public class Settings {
     /********************
      ****** Eh
      ********************/
+
+    public static final String KEY_THEME = "theme";
+    public static final int THEME_LIGHT = 0;
+    public static final int THEME_DARK = 1;
+    public static final int THEME_BLACK = 2;
+    private static final int DEFAULT_THEME = THEME_LIGHT;
+
+    public static int getTheme() {
+        return getIntFromStr(KEY_THEME, DEFAULT_THEME);
+    }
+
     public static final String KEY_GALLERY_SITE = "gallery_site";
     private static final int DEFAULT_GALLERY_SITE = 1;
 
